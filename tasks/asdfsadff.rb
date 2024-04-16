686abe14477ca54248b8fa4c54ffd240a45c532e
UserLeftBlank <m3tub326@gmail.com>
2024-04-16 11:01:32 -0400

    Antonio T5_q1

--- tasks/asdfsadff.rb
+++ tasks/asdfsadff.rb
@@ -1,1 +1,39 @@
-asdfsadff
+#!/opt/local/bin/ruby
+#Copyright 2012 Joseph Bergin
+#License: Creative Commons Attribution-Noncommercial-Share Alike 3.0 United States License
+
+require_relative "../karel/ur_robot.rb"
+
+# a task for a stair sweeper
+def task()
+  world = Robota::World
+  world.read_world("../worlds/beepers.txt")
+  
+  karel = ur_robot.new(3, 3, North, 5)
+  karel.put_beeper
+  karel.move
+  karel.put_beeper
+
+  karel.move
+  karel.put_beeper
+  karel.move
+
+  karel.put_beeper
+  karel.move
+  karel.put_beeper
+
+  karel.turn_off()
+
+  
+end
+
+if __FILE__ == $0
+  if $graphical
+     screen = window(8, 10) # (size, speed)
+     screen.run do
+       task()
+     end
+   else
+     task()
+   end
+end
