c77be65c9feef6bcb2a734ef2cbb33229231fb3a
UserLeftBlank <m3tub326@gmail.com>
2024-04-18 10:17:17 -0400

    a

---
+++ tasks/T5_q1_main.rb
@@ -0,0 +1,37 @@
+#!/opt/local/bin/ruby
+#Copyright 2012 Joseph Bergin
+#License: Creative Commons Attribution-Noncommercial-Share Alike 3.0 United States License
+
+$graphical = true
+
+require_relative "ur_robot"
+require_relative "../karel/robota"
+
+# a task for a stair sweeper
+def task()
+
+  karel = UrRobot.new(3, 3, Robota::East, 5)
+  karel.put_beeper
+  karel.move
+  karel.put_beeper
+
+  karel.move
+  karel.put_beeper
+  karel.move
+
+  karel.put_beeper
+  karel.move
+  karel.put_beeper
+  
+ end
+
+if __FILE__ == $0
+  if $graphical
+     screen = window(8, 40) # (size, speed)
+     screen.run do
+       task
+     end
+   else
+     task
+   end
+end
