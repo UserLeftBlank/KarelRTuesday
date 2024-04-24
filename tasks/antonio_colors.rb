12362b66e31da5d20449f71f6df8e6a9df660e8b
UserLeftBlank <m3tub326@gmail.com>
2024-04-24 10:01:12 -0400

    T8 done

--- tasks/antonio_colors.rb
+++ tasks/antonio_colors.rb
@@ -11,5 +11,24 @@ class AntonioColors < UrRobot
   def initialize (street, avenue, direction, beepers, color)
     super(street, avenue, direction, beepers, color)
    end
+   def move_2
+ 	 move
+     move
+     end
+    def quarter_lap
+     pick_beeper
+     move_2
+     move_2
+     move
+     put_beeper
+      end
 
+    def turn_quarter_lap
+     turn_right
+     pick_beeper
+     move_2
+     move_2
+     move
+     put_beeper
+      end
  end
--- tasks/t8_q3_antonio_main.rb
+++ tasks/t8_q3_antonio_main.rb
@@ -10,9 +10,34 @@ require_relative "../karel/robota"
 def task()
 
   karel = AntonioColors.new(1, 1, Robota::NORTH, 1, :red)
+  karel2 = AntonioColors.new(6, 1, Robota::EAST, 0, :blue)
+  karel3 = AntonioColors.new(6, 6, Robota::SOUTH, 0, :green)
+  karel4 = AntonioColors.new(1, 6, Robota::WEST, 0, :yellow)
+  karel5 = AntonioColors.new(1, 1, Robota::NORTH, 0, :black)
+  karel.move_2
+  karel.move_2
+  karel.move
 
-  karel2 = AntonioColors.new(1, 1, Robota::NORTH, 0, :red)
+  karel.put_beeper
+  karel2.quarter_lap
+  karel3.quarter_lap
+  karel4.quarter_lap
+  karel5.quarter_lap
 
+  karel.turn_quarter_lap
+  karel2.turn_quarter_lap
+  karel3.turn_quarter_lap
+
+  karel4.turn_quarter_lap
+  karel5.turn_quarter_lap
+  karel.turn_quarter_lap
+
+  karel2.turn_quarter_lap
+  karel3.turn_quarter_lap
+  karel4.turn_quarter_lap
+
+  karel5.turn_quarter_lap
+  karel.turn_quarter_lap
  end
  
 if __FILE__ == $0
